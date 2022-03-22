d57f625dfe1f34633d56bb95b37b34d95d3e342b
Peter Gemmell <peter.gemmell@mths.ca>
2022-03-22 18:00:42 +0000

    malfeasance

--- pizza_Constants.cpp
+++ pizza_Constants.cpp
@@ -2,32 +2,28 @@
 
 // Created by Peter Gemmell
 // Created on March 2022
-// This program calculates the cost of pizza using constants
+// This program is a simple guessing game
 
 #include <iostream>
-#include <iomanip>
-int main() {
-    // This function calculates the cost of a pizza
-    const float RENT = 1;
-    const float LABOUR = 0.75;
-    const float COST_PER_INCH = 0.5;
-    const float HST = 0.13;
 
-    float diameter;
-    float sub_total;
-    float total;
+int main() {
+    // This function is a simple guessing game
+    const float mystery_number = 7;
+    int guess_number;
 
     // Input
-    std::cout << "What diameter pizza would you like? (inch): ";
-    std::cin >> diameter;
+    std::cout << "Guess a number between 0-9 : ";
+    std::cin >> guess_number;
 
-    // Process
-    sub_total = RENT + LABOUR + (diameter * COST_PER_INCH);
-    total = sub_total * (1 + HST);
-
-    // Output
-    std::cout << "The final cost is: $" << std::fixed
-    << std::setprecision(2) << std::setfill('0') << total << std::endl;
-    std::cout << std::endl;
-    std::cout << "Done." << std::endl;
+    // Process & Output
+    if (guess_number == mystery_number) {
+        std::cout << "You guessed correctly!";
+            std::cout << std::endl;
+            std::cout << "Done." << std::endl;
+    }
+    if (guess_number != mystery_number) {
+        std::cout << "You Guessed Incorrectly." << std::endl;
+        std::cout << std::endl;
+        std::cout << "Done." << std::endl;
+    }
 }
